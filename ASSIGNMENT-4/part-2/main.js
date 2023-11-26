f0a6c4b7825a1cd1ccfdf0ef91d361937dab1cfa
Rahul01259 <rahul30@dcmail.ca>
2023-11-25 19:56:11 -0500

    commit-3
    images list created and alt text added

--- ASSIGNMENT-4/part-2/main.js
+++ ASSIGNMENT-4/part-2/main.js
@@ -5,8 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageslist = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 
 /* Declaring the alternative text for each image file */
+const alternativetext = {
+    'pic1.jpg' : 'eye image',
+    'pic2.jpg' : 'rocks image',
+    'pic3.jpg' : 'flowers image',
+    'pic4.jpg' : 'art on wall',
+    'pic5.jpg' : ' moth on a leaf'
+  }
 
 /* Looping through images */
 
