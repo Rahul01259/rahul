d1eb141e3f7e4ec307eb08ba317824d91c7528ab
Rahul01259 <rahul30@dcmail.ca>
2023-11-25 19:39:18 -0500

    commit-4
    changes made

--- ASSIGNMENT-4/type-1/raw.js
+++ ASSIGNMENT-4/type-1/raw.js
@@ -7,30 +7,42 @@ function randomValueFromArray(array){
 }
 
 
-var Text = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+var textarray = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-var insertx = ["Willy the Goblin","Big Daddy","Father Christmas"];
+var insertx= ["Willy the Goblin","Big Daddy","Father Christmas"];
 
 var inserty = ["the soup kitchen","Disneyland","the White House"];
 
 var insertz = ["spontaneously combusted","melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-
 randomize.addEventListener('click', result);
 
 function result() {
+   var result = textarray;
 
-  if(customName.value !== '') {
-    const name = customName.value;
+var x, y, z;
 
+x= randomValueFromArray(insertx);
+y= randomValueFromArray(inserty);
+z = randomValueFromArray(insertz);
+
+result= result.replace(":insertx:",x);
+result = result.replace(":inserty:",y);
+result = result.replace(":insertz:",z);
+
+  
+  if(customName.value != '') {
+    var name = customName.value;
+    result = result.replace('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+    
+    var temperature =  Math.round((94-32)*5/9)+'celcius';
+    result= result.replace("94 farenheit", temperature);  
+    result = result.replace("300 pounds", weight); 
   }
-
-  story.textContent = ;
+  
+  story.textContent = result;
   story.style.visibility = 'visible';
 }
